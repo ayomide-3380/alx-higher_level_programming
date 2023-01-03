0750a6dcaeb99ccbdbcf81cb5aa965b3f895a09b
ayomide-3380 <alaoayomide700@gmail.com>
2023-01-03 22:12:09 +0100

    add files

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uniq(list):
+    """ Returns unique values of a list """
+    u_list = []
+    for item in list:
+        if item not in u_list:
+            u_list.append(item)
+    return u_list
