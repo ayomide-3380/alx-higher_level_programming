df3449ba36a96b0fa9423051e3cc135daa48ece3
ayomide-3380 <alaoayomide700@gmail.com>
2023-01-16 12:29:58 +0100

    add files

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 
-def uniq(list):
-    """ Returns unique values of a list """
-    u_list = []
-    for item in list:
-        if item not in u_list:
-            u_list.append(item)
-    return u_list
+class Base():
+    """ My base class """
+
+    __nb_instances = 0
+
+    def __init__(self):
+        Base.__nb_instances += 1
+        self.id = Base.__nb_instances
+
+class User(Base):
+    """ My User class """
+
+    def __init__(self):
+        super().__init__()
+
+u = User()
+print(u.id)
